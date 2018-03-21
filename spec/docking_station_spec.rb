4bd4bee1222af612de38462bb83e8216b61f6bf7
Mona Al-Haery <monadrums@gmail.com>
2018-03-21 15:09:47 +0000

    step 8 - created unit test and method release_bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,11 @@
 require "Docking_Station"
 
-describe DockingStation do
+# describe DockingStation do
+#   it "expected to respond to release_bike" do
+#   expect(DockingStation.new).to respond_to (:release_bike)
+#   end
+# end
 
-end
+describe DockingStation.new do
+  it { is_expected.to respond_to (:release_bike) }
+end 
