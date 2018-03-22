08755285592d49a910ad4199399bcd80501f2b61
Marwa Elali <marwaelali@gmail.com>
2018-03-22 16:02:32 +0000

    challenge 12 solved

--- lib/Docking_Station.rb
+++ lib/Docking_Station.rb
@@ -3,7 +3,8 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    @bike = Bike.new
+    fail "No bikes available" unless @bike
+    @bike
   end
 
   def dock_bike(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,43 +1,40 @@
 require "docking_station"
 require "bike"
 
-# describe DockingStation do
-#   it "expected to respond to release_bike" do
-#   expect(DockingStation.new).to respond_to (:release_bike)
-#   end
-# end
-
 describe DockingStation do
 
-  docking_station = DockingStation.new
-
   it "expected to respond to release_bike" do
-   is_expected.to respond_to (:release_bike)
+   is_expected.to respond_to(:release_bike)
   end
 
   it "gets a bike when passed release_bike" do
-   expect(docking_station.release_bike).to be_an_instance_of(Bike)
+    station = DockingStation.new
+    station.dock_bike(Bike.new)
+   expect(station.release_bike).to be_an_instance_of(Bike)
   end
 
   it "returns true if bike is working" do
-    expect(docking_station.release_bike.working?).to be(true).or be(false)
+    station = DockingStation.new
+    station.dock_bike(Bike.new)
+    expect(station.release_bike.working?).to be(true).or be(false)
   end
 
   it "expected to respond to dock_bike" do
-     is_expected.to respond_to (:dock_bike)
+     is_expected.to respond_to(:dock_bike)
   end
 
   it "expected to respond to dock_bike" do
-     expect(docking_station).to respond_to(:dock_bike).with(1).argument
+     expect(DockingStation.new).to respond_to(:dock_bike).with(1).argument
   end
 
-  it "should show docking station status" do
-    expect(docking_station.bike).to be_an_instance_of(Bike)
+  it "should raise an error when bikes are not available" do
+    expect{ DockingStation.new.release_bike }.to raise_error("No bikes available")
+  end
+
+  it "should return a bike when a bike is available" do
+    docking_station = DockingStation.new
+    docking_station.dock_bike(Bike.new)
+    expect(docking_station.release_bike).to be_an_instance_of(Bike)
   end
 
 end
-
-
-
-# Add a test to your DockingStation specification that
-# a) gets a bike, and then b) expects the bike to be working
