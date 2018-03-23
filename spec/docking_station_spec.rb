82e726c4aa8fe0bb8ab772fd2b2d837c8d4e608f
Lorraine Philllips <ljcphilllips@gmail.com>
2018-03-23 17:03:20 +0000

    challenge 19

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,17 @@
 require "docking_station"
 require "bike"
 
+
+
+# it 'releases working bikes' do
+#   allow(bike).to receive(:working?).and_return(true)
+#    subject.dock(bike)
+#     released_bike = subject.release_bike
+#       expect(released_bike).to be_working
+
+
 describe DockingStation do
+  let(:bike) { double :bike}
   it "should capacity for a new instance of DockingStation" do
     docking_station = DockingStation.new(30)
     expect(docking_station.capacity).to eq 30
@@ -17,7 +27,7 @@ describe DockingStation do
   end
 
   it "gets a bike when passed release_bike" do
-    subject.dock_bike double (:bike)
+    subject.dock_bike Bike.new
     expect(subject.release_bike).to be_an_instance_of(Bike)
   end
   end
@@ -25,7 +35,8 @@ describe DockingStation do
   describe "#dock_bike" do
 
   it "returns true if bike is working" do
-    subject.dock_bike double (:bike)
+    allow(bike).to receive(:working).and_return(true)
+    subject.dock_bike bike
     expect(subject.release_bike.working).to be(true).or be(false)
   end
 
@@ -38,18 +49,19 @@ describe DockingStation do
   end
 
   it "should raise an error when docking_station is full" do
-    (DockingStation::DEFAULT_CAPACITY).times { subject.dock_bike double (:bike)}
-    expect {subject.dock_bike double (:bike)}.to raise_error("Docking Station is full")
+    (DockingStation::DEFAULT_CAPACITY).times { subject.dock_bike bike}
+    expect {subject.dock_bike bike}.to raise_error("Docking Station is full")
   end
 
 
   it "should return a bike when a bike is available" do
-    subject.dock_bike double (:bike)
+    subject.dock_bike Bike.new
     expect(subject.release_bike).to be_an_instance_of(Bike)
   end
 
   it "allows to report a bike as broken" do
-    bike = double (:bike)
+    allow(bike).to receive(:working=).with(false)
+    allow(bike).to receive(:working).and_return(false)
     subject.dock_bike(bike, true)
     expect(bike.working).to eq false
   end
